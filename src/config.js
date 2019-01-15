fabf7608f9c7d48d5f35a062bda1f5a2867bb712
João Victor Bechara <joaovictor.bechara@gmail.com>
2019-01-15 01:02:21 -0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,5 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo!';
 module.exports = {
     connectionString: 'mongodb://jvbechara:crvg25101995@ds048719.mlab.com:48719/ndstr',
     sendgridKey: 'SUA CHAVE AQUI', // pra enviar email
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=jvbechara;AccountKey=BqpKsF4F5wOlaanFiJ6BfOYErD5jkDjZ4YFsYOM47IMWzcMYjGEVcjFcpNU6V0WSAgnXaJW4meiaX+6yN9b9rg==;EndpointSuffix=core.windows.net' // pra armazenar as imagens dos produtos
+    containerConnectionString: 'CHAVE' // pra armazenar as imagens dos produtos
 }
-
-// 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

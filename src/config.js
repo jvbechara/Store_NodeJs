e77a81b4ca31194d4928c08e7e28337c2fcd2a3d
João Victor Bechara <joaovictor.bechara@gmail.com>
2019-03-28 17:15:01 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.SALT_KEY = 'salt';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo!';
 
 module.exports = {
-    connectionString: 'mongodb://jvbechara:crvg25101995@ds048719.mlab.com:48719/ndstr',
+    connectionString: 'connectString',
     sendgridKey: 'SUA CHAVE AQUI', // pra enviar email
     containerConnectionString: 'CHAVE' // pra armazenar as imagens dos produtos
 }
